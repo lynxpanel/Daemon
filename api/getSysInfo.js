6f0f7683d23f7d44429c099251090a3fa16b5a86
KenleyundLeon <muthkenley6@gmail.com>
2024-05-15 21:49:27 +0200

    modified:   api/getSysInfo.js

--- api/getSysInfo.js
+++ api/getSysInfo.js
@@ -1,4 +1,5 @@
-const os = require('os-utils');
+const os = require('node:os');
+const osu = require('os-utils');
 
 module.exports = {
   name: "SysInfo",
@@ -6,8 +7,8 @@ module.exports = {
   path: "/api/information",
   callback: async (req, res) => {
     try {
-      const cpuUsage = await new Promise(resolve => {os.cpuUsage(v => {resolve((v * 100).toFixed(2));});});
-      const ramUsage = os.totalmem() - os.freemem();
+      const cpuUsage = await new Promise(resolve => {osu.cpuUsage(v => {resolve((v * 100).toFixed(2));});});
+      const ramUsage = osu.totalmem() - osu.freemem();
       res.status(200).json({ "CPU": cpuUsage + "%", "RAM": ramUsage.toFixed(0) + 'MB' });
     } catch (error) {
       console.error(error);
