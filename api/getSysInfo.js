7fc6c87d5ba4d51264cb441fcfd2f2873d46b513
KenleyundLeon <muthkenley6@gmail.com>
2024-05-15 21:47:58 +0200

    modified:   api/getSysInfo.js

--- api/getSysInfo.js
+++ api/getSysInfo.js
@@ -1,24 +1,17 @@
 const os = require('os-utils');
 
-
 module.exports = {
   name: "SysInfo",
   method: "GET",
   path: "/api/information",
   callback: async (req, res) => {
     try {
-      const cpuUsage = await new Promise(resolve => {
-        os.cpuUsage(v => {
-          resolve((v * 100).toFixed(2));
-        });
-      });
-      res.status(200).json({ CPU: `${cpuUsage}%` });
-      console.log(`CPU usage: ${cpuUsage}%`);
+      const cpuUsage = await new Promise(resolve => {os.cpuUsage(v => {resolve((v * 100).toFixed(2));});});
+      const ramUsage = os.totalmem() - os.freemem();
+      res.status(200).json({ "CPU": cpuUsage + "%", "RAM": ramUsage.toFixed(0) + 'MB' });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Failed to retrieve system information' });
     }
-
   }
-
 }
