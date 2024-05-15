720a34010b138e3555a5e52f51c579f87019953d
KenleyundLeon <muthkenley6@gmail.com>
2024-05-15 22:41:34 +0200

    modified:   api/getSysInfo.js 	modified:   package-lock.json 	modified:   package.json

--- api/getSysInfo.js
+++ api/getSysInfo.js
@@ -1,5 +1,4 @@
-const os = require('node:os');
-const osu = require('os-utils');
+const osu = require('node-os-utils');
 
 module.exports = {
   name: "SysInfo",
@@ -7,12 +6,29 @@ module.exports = {
   path: "/api/information",
   callback: async (req, res) => {
     try {
-      const cpuUsage = await new Promise(resolve => {osu.cpuUsage(v => {resolve((v * 100).toFixed(2));});});
-      const ramUsage = osu.totalmem() - osu.freemem();
-      res.status(200).json({ "CPU": cpuUsage + "%", "RAM": ramUsage.toFixed(0) + 'MB' });
+        const cpuUsage = await new Promise(resolve => {osu.cpu.usage().then(v => {resolve(v);});});
+        const ramTotal = osu.mem.totalMem() / 1024 / 1024;
+        const ramUsage = await new Promise(resolve => {osu.mem.used().then(v => {resolve(v.usedMemMb);});});
+        const ramFree = await new Promise(resolve => {osu.mem.free().then(v => {resolve(v.freeMemMb);});});
+        const driveTotal = await new Promise(resolve => {osu.drive.info().then(v => {resolve(v.totalGb)})});
+        const driveUsage = await new Promise(resolve => {osu.drive.used().then(v => {resolve(v.usedGb);});});
+        const driveFree = await new Promise(resolve => {osu.drive.free().then(v => {resolve(v.freeGb);});});
+        res.status(200).json({
+            "CPU": cpuUsage + "%",
+            "RAM": {
+                "TotalMB": ramTotal.toFixed(0) + 'MB',
+                "UsageMB": ramUsage.toFixed(0) + 'MB',
+                "FreeMB": ramFree.toFixed(0) + 'MB',
+            },
+            "DRIVE": {
+                "TotalGB": driveTotal + 'GB',
+                "UsageGB": driveUsage + 'GB',
+                "FreeGB": driveFree + 'GB',
+            }
+        });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to retrieve system information' });
+        console.error(error);
+        res.status(500).json({ error: 'Failed to retrieve system information' });
     }
   }
 }
