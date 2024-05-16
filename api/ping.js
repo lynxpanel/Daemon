b82bb65d8cd57c9318dfee0cee46b17e7a77e2be
c4g7 <141867000+c4g7-dev@users.noreply.github.com>
2024-05-16 16:45:56 +0200

    updated api

--- api/ping.js
+++ api/ping.js
@@ -1,8 +1,12 @@
+const { os } = require("node-os-utils");
+
 module.exports = {
     name: "ping",
     method: "GET",
     path: "/api/ping",
     callback: (req, res, dir) => {
-        res.status(200).json({ status: 'SUCCESS', message: "Pong!" });
+        res.status(200).json({ status: 'SUCCESS', message: "Pong!"});
+
+        // Socket code has been removed since it was not provided and might not be relevant to this specific callback function
     },
 };
